c0455b83b9df116427492db53eb75288ae238f91
Santiago Figueroa <SantiagoIsaacGIT@gmail.com>
2024-11-01 17:20:45 -0300

    ee

--- proyecto-final/src/components/Olys/Nutricionista/Nutricionista.jsx
+++ proyecto-final/src/components/Olys/Nutricionista/Nutricionista.jsx
@@ -83,7 +83,7 @@ const Nutricionista = () => {
                 </div>
             </div>
 
-            <div className="mt-[1000px]"></div>
+            <div className="mt-[1002px]"></div>
         </>        
     );
 };
